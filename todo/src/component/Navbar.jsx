7f89162ed7c0768d89e0b7aaeeb932ed257d417d
Adilya <adilya.kadyrbekova.00@gmail.com>
2025-07-01 16:13:44 +0600

    add useContext

--- todo/src/component/Navbar.jsx
+++ todo/src/component/Navbar.jsx
@@ -1,8 +1,9 @@
 import { Link } from "react-router-dom";
+import { IoMdNotificationsOutline } from "react-icons/io";
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-800 p-4">
+    <nav className="bg-gray-800 p-4 flex justify-around">
       <ul className="flex justify-center text-xl space-x-4">
         <Link to="/" className="text-white hover:text-gray-300">
           Главная
@@ -11,6 +12,7 @@ const Navbar = () => {
           TODO
         </Link>
       </ul>
+      <IoMdNotificationsOutline className="flex justify-center text-3xl	 text-gray-300" />
     </nav>
   );
 };
